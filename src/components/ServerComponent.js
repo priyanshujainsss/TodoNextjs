3d752887a57a933e372c60dd506380fe8423c360
priyanshujainsss <20jainpriyanshu@gmail.com>
2023-06-25 16:14:17 +0530

    update created date

--- src/components/ServerComponent.js
+++ src/components/ServerComponent.js
@@ -7,7 +7,7 @@ export const TodoItem = ({title,description,id, completed,date,updateAt}) => {
         <div>
             <h4>{title}</h4>
             <p>{description}</p>
-            <p>Created At:  {new Date(date).toLocaleString()}</p>
+            <p>Created At:  {new Date(date).toLocaleDateString()} {new Date(date).toLocaleTimeString()}</p>
             <p>Updated At: {new Date(updateAt).toLocaleString()} </p>
         </div>
         <TodoBtn id={id} completed={completed} />
